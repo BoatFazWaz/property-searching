cfbe62438f5aba82aeb64e54026d343cfeb2c6f5
BoatFazWaz <boat@fazwaz.com>
2025-04-20 01:48:22 +0700

    Update package dependencies in package.json and package-lock.json, downgrade autoprefixer, postcss, and tailwindcss versions for compatibility, and remove unused Tailwind CSS configuration file to streamline project structure.

--- html/vite.config.js
+++ html/vite.config.js
@@ -25,9 +25,4 @@ export default defineConfig({
             vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
-    css: {
-        postcss: {
-            plugins: [],
-        },
-    },
 });
